f163183f29590ca4cb13f64cae7738ddfeafebd9
RobertoRuben <robertoch263@gmail.com>
2025-09-13 15:29:50 -0500

    ✨ feat(student): add get_pageable_students method to IStudentRepository for paginated student retrieval

--- src/app/domain/students/repository/interface/student_repository.py
+++ src/app/domain/students/repository/interface/student_repository.py
@@ -1,10 +1,29 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from src.app.core.model.page import Page
 from src.app.core.repository.interface import IBaseRepository
 from src.app.domain.students.model import Student
+
 
 class IStudentRepository(IBaseRepository[Student], ABC):
     """
     Interface for student repository.
     """
-    
-    pass
+
+    @abstractmethod
+    async def get_pageable_students(
+        self,
+        page: int,
+        size: int,
+    ) -> Page:
+        """
+        Get paginated students with only grade_name and section_name included.
+        
+        Args:
+            page: Page number
+            size: Items per page
+            where_conditions: Optional filter conditions
+            
+        Returns:
+            Page: Paginated students with grade_name and section_name
+        """
+        pass
