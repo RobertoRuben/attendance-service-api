89e92814413043f495a05ab9a0247c37aa46203f
RobertoRuben <robertoch263@gmail.com>
2025-09-14 00:49:32 -0500

    ✨ feat(student): agregar StudentPageResponseDTO para respuestas paginadas de estudiantes

---
+++ src/app/domain/students/dto/response/__init__.py
@@ -0,0 +1,7 @@
+from .student_page_response_dto import StudentPageResponseDTO
+from .student_response_dto import StudentResponseDTO
+
+__all__ = [
+    "StudentResponseDTO",
+    "StudentPageResponseDTO",
+]
