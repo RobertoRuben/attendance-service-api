8b4fcbd9f95dd8dab9623aa1ef9c5aec5c6f243b
RobertoRuben <robertoch263@gmail.com>
2025-09-15 15:11:55 -0500

    ✨ feat(main): agregar student_router a la configuración de rutas de la API

--- src/app/main.py
+++ src/app/main.py
@@ -6,12 +6,19 @@ from src.app.core.exception import register_exception_handlers
 from src.app.domain.root.controller import root_router, root_tags_metadata
 from src.app.domain.classrooms.controller import grade_router, grades_tags_metadata
 from src.app.domain.classrooms.controller import section_router, sections_tags_metadata
+from src.app.domain.students.controller import student_router, students_tags_metadata
 from src.app.core.config.scalar_config import configure_scalar_route
 from src.app.core.monitoring.controller import health_router, health_tags_metadata
 
 API_PREFIX = "/api/v1"
 
-tags_metadata = [grades_tags_metadata, sections_tags_metadata, health_tags_metadata, root_tags_metadata]
+tags_metadata = [
+    grades_tags_metadata,
+    sections_tags_metadata,
+    health_tags_metadata,
+    root_tags_metadata,
+    students_tags_metadata,
+]
 
 
 @asynccontextmanager
@@ -49,9 +56,9 @@ def create_app() -> FastAPI:
     app.include_router(health_router, prefix=API_PREFIX)
     app.include_router(grade_router, prefix=API_PREFIX)
     app.include_router(section_router, prefix=API_PREFIX)
+    app.include_router(student_router, prefix=API_PREFIX)
 
     return app
 
 
 app = create_app()
-
